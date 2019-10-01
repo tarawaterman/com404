101a16870d68752726dbc8b695e4bc0be7e99a8b
tarawaterman <tara.waterman689@gmail.com>
2019-10-01 13:42:10 +0000

    Added code to demonstrate the use of comparison operators.

---
+++ 1-basics/3-decision/5-comparison-operators/bot.py
@@ -0,0 +1,10 @@
+print("Enter the first number.")
+first_number = input()
+print("Enter the second number.")
+second_number = input()
+if  (first_number > second_number):
+    print("The second number is the smallest")
+elif (first_number < second_number):
+    print("The first number is the smallest")
+elif (first_number == second_number):
+    print("Both are equal!")
