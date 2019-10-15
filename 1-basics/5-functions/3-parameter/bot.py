67f94455704a0018aa54711ac59b15259e1ca2bd
tarawaterman <tara.waterman689@gmail.com>
2019-10-15 13:44:16 +0000

    Redo

---
+++ 1-basics/5-functions/3-parameter/bot.py
@@ -0,0 +1,9 @@
+def escape_by(plan):
+    print(plan)
+
+escape_by("jumping over")
+print("We cannot escape that way! The boulder is too big!")
+escape_by("running around")
+print("We cannot escape that way! The boulder is moving too fast!")
+escape_by("going deeper")
+print("That might just work! Let's go deeper!")
