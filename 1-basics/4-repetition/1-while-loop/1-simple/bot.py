882ecf048ce17b0b88a0a7073c90b22b57d7e635
tarawaterman <tara.waterman689@gmail.com>
2019-10-15 14:07:17 +0000

    Added code to demonstrate the use of a simple while loop

---
+++ 1-basics/4-repetition/1-while-loop/1-simple/bot.py
@@ -0,0 +1,10 @@
+print("How many cables should I remove?")
+cables_to_remove = int(input())
+
+cables_removed = 0
+
+print()
+
+while(cables_removed < cables_to_remove):
+    print("Removed cable.")
+    canbles_removed = cables_removed + 1
