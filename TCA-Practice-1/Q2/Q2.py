51a2b815d90e76447a3a858afe2c9d6584b18266
tarawaterman <tara.waterman689@gmail.com>
2019-10-22 13:20:28 +0000

    TCA practice question 2.

---
+++ TCA-Practice-1/Q2/Q2.py
@@ -0,0 +1,8 @@
+print("Where is Forky?")
+where = input()
+if  (where == "With Bonnie"):
+    print("Phew! Bonnie will be happy.")
+elif (where == "Running away"):
+    print("Oh no! Bonnie will be upset!")
+else:
+    print("Ah! I better look for him.")
