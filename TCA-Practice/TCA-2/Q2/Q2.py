129b63a312406e64f366f57ceadfc29e17d98e65
tarawaterman <tara.waterman689@gmail.com>
2019-10-22 13:47:22 +0000

    TCA 2 practice question 2.

---
+++ TCA-Practice/TCA-2/Q2/Q2.py
@@ -0,0 +1,6 @@
+print("Have you fear in your heart?")
+fear = input()
+if  (fear == "yes"):
+    print("Fear is the path to the dark side. You cannot be a Jedi apprentice.")
+else:
+    print("The force is strong in you. You may be a Jedi apprentice.")
